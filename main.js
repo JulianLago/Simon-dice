0c76bcbd3aad3d0a8dd815138bb36b747545583f
julian lago <julianlago93@gmail.com>
2020-05-29 16:46:51 -0300

    Edita estilos con CSS y Bootstrap
    edita el posicionamiento de los elementos y su diseño.

--- main.js
+++
@@ -1,8 +0,0 @@
-const $estadoJuego = document.getElementById("mensaje-estado");
-$estadoJuego.innerText = "Empezar";
-
-const $botonEmpezarJuego=document.getElementById("empezar-juego");
-$botonEmpezarJuego.onclick=function(){
-    $estadoJuego.innerText="Turno PC";
-
-}
